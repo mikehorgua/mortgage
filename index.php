6ccf6ecb59f8c2cd279d2acce247acda64bd76b6
mikehorgua <m.hrabynskyi@gmail.com>
2021-09-25 14:22:01 +0300

    25.09

--- index.php
+++ index.php
@@ -209,10 +209,10 @@ If ($page=='calc') {
 
 <!-- W3.CSS Container -->
 <div class="w3-light-grey w3-container w3-padding-32" style="margin-top:75px;padding-right:58px">
-    <p class="w3-left">
+    <p class="w3-right">
         Designed by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-opacity">w3.css</a>
     </p>
-    <p class="w3-left">
+    <p class="w3-right">
         Mykhailo Hrabynskyi for ElifTech IT School Test task
     </p>
 </div>
