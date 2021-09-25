8f194679fc7436f196b62e49c862080862576ccd
mikehorgua <m.hrabynskyi@gmail.com>
2021-09-25 15:08:14 +0300

    25.09

--- mysql.php
+++ mysql.php
@@ -1,9 +1,12 @@
-<!--
 <?php
 $db_location = "db.tzk612.nic.ua";
 $db_user = "mikehorg_mortgage";
 $db_password = "975864";
 $db_name = "mikehorg_mortgage";
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli = new mysqli($db_location, $db_user, $db_password, $db_name);
+/*
 $db_cnx = @mysql_connect($db_location,$db_user,$db_password);
 if (!$db_cnx) {
 // if descriptor = 0 then not connected
@@ -15,7 +18,6 @@ if (!@mysql_select_db($db_name,$db_cnx)) {
     echo "<p>Виникли проблеми із під'єднуванням бази даних.</p>";
     exit ();
 }
-
+*/
 ?>
--->
 
